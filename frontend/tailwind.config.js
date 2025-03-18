3f16dad77cd9654b092f4c352214f1140db88dae
Charles Morman <github@cmorman.com>
2025-03-18 21:57:12 +0000

    chore: initial project frontend setup - Vite/React/JS - TailwindCSS - PostCSS - Build Tasks

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./frontend/index.html",
+    "./frontend/src/**/*.{js,ts,jsx,tsx}",
+    "./frontend/src/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
