de6946c6267e2b7004431edd88b2a0b820cf53e4
Charles Morman <github@cmorman.com>
2025-03-26 05:03:45 +0000

    feat: update ChatIntro component to use large icon and display model ID

--- frontend/src/components/ChatIntro.jsx
+++ frontend/src/components/ChatIntro.jsx
@@ -1,18 +1,24 @@
 import useModelParser from "../hooks/useModelParser";
 
 const ChatIntro = ({ modelId }) => {
-  const { _, paramCount, arch, Icon, IconCombined } = useModelParser(modelId);
+  const model = useModelParser(modelId);
+  const { _, paramCount, arch, Icon, IconCombined, IconCombinedLg } = model;
   return (
     <div
       className="
       flex flex-col items-center
+      px-30 py-10
+      rounded-4xl
         font-inter
+        bg-gray-50
+        shadow-xl shadow-black/20
       "
-      >
-          <div>Current Model:</div>
-      <div className="scale-250 m-12">
-        <IconCombined />
+    >
+      <div>Current Model:</div>
+      <div className="m-6">
+        <IconCombinedLg />
       </div>
+      <div className="italic">Model: {model.modelId}</div>
     </div>
   );
 };
