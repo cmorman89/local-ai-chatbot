07db4bf333ba39d2084ef01d7c1eb64e8b86e5a9
Charles Morman <github@cmorman.com>
2025-03-30 01:57:35 +0000

    feat: Update MainContentWindow styles for improved layout and animation

--- frontend/src/components/MainContentWindow.jsx
+++ frontend/src/components/MainContentWindow.jsx
@@ -3,12 +3,13 @@ import PropTypes from "prop-types";
 const MainContentWindow = ({ children }) => {
   return (
     // <div className="flex w-full h-full relative pl-24 pb-40">
-    <div className="flex w-full h-full relative pl-24">
+    <div className="flex w-full h-full relative pl-14 md:pl-24">
       <div
         className="
-        h-40 w-screen
+        h-16 lg:h-40 w-screen
         fixed bottom-0 left-0 z-20
         bg-gradient-to-b from-25% to-75% from-transparent to-white
+        animate animate-fade-up-slow
         "
       ></div>
       {children}
