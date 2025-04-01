c1220f84efdd91a4d676ee1f64eb648943072428
Charles Morman <github@cmorman.com>
2025-04-01 05:48:13 +0000

    feat: enhance MainContentWindow with dark mode transition effects and update Home styles

--- frontend/src/components/MainContentWindow.jsx
+++ frontend/src/components/MainContentWindow.jsx
@@ -1,8 +1,18 @@
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
-const MainContentWindow = ({ children }) => {
+const MainContentWindow = ({ children, darkMode }) => {
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  // When darkMode changes, make isTransitioning true for 1000ms
+  useEffect(() => {
+    setIsTransitioning(true);
+    const timer = setTimeout(() => {
+      setIsTransitioning(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [darkMode]);
   return (
-    // <div className="flex w-full h-full relative pl-24 pb-40">
     <div
       className="flex w-full h-full
         pl-14 md:pl-24
@@ -11,12 +21,14 @@ const MainContentWindow = ({ children }) => {
     >
       {children}
       <div
-        className="
+        className={`
         h-16 lg:h-40 w-screen
         fixed bottom-0 left-0 z-19
-        bg-gradient-to-b from-25% to-75% from-transparent to-white
-        animate animate-fade-up-slow
-        "
+        bg-gradient-to-b from-25% to-75% from-transparent 
+        ${isTransitioning ? "opacity-0" : "opacity-100"}
+        ${darkMode ? "to-gray-700" : "to-gray-200"}
+        animate 
+        `}
       ></div>
     </div>
   );
